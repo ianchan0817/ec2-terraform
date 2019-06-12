dd55845b5d8229a824519e479fa8eb94d4fbde59
Ian Chan <yingchungchan@yahoo.com.hk>
2019-06-13 00:29:40 +0800

    feat(var): add variables.tf and use variable in vpc

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "name" {
+  description = "Name to be used on all the resources as identifier"
+  default     = ""
+}
+
+variable "cidr" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overridden"
+  default     = "172.31.0.0/16"
+}
+
+variable "instance_tenancy" {
+  description = "A tenancy option for instances launched into the VPC"
+  default     = "default"
+}
+
+variable "enable_dns_hostnames" {
+  description = "Should be true to enable DNS hostnames in the VPC"
+  default     = true
+}
+
+variable "enable_dns_support" {
+  description = "Should be true to enable DNS support in the VPC"
+  default     = true
+}
+
+variable "assign_generated_ipv6_cidr_block" {
+  description = "Requests an Amazon-provided IPv6 CIDR block with a /56 prefix length for the VPC. You cannot specify the range of IP addresses, or the size of the CIDR block"
+  default     = false
+}
+
+variable "vpc_tags" {
+  description = "Additional tags for the VPC"
+  default     = "Ian"
+}
--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "default" {
-  cidr_block = "172.31.0.0/16"
-  enable_dns_hostnames = true
+  cidr_block = "${var.cidr}"
+  enable_dns_hostnames = "${var.enable_dns_hostnames}"
   tags {
-      Name = "Ian"
+      Name = "${var.vpc_tags}"
   }
 }
